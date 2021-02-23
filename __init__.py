7300d44d21f2c5c1d84ca439623130e38e0389e5
Mohit <Mohit@MacBook.local>
2021-02-23 18:36:26 +0530

    temp edit

--- __init__.py
+++ __init__.py
@@ -13,7 +13,5 @@ def index():
 
         short = ''.join([[i for i in random.choice([i for i in random.choice([string.ascii_lowercase, string.ascii_uppercase, string.digits])])][0] for _ in range(6)])
 
-
-
         return render_template('index.html', short=short)
     return render_template('index.html')
