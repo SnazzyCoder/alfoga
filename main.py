20fbf88966e488399b44bb382912eea056942b7a
Mohit <Mohit@MacBook.local>
2021-02-24 19:39:57 +0530

    change

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, request
+from flaskext.mysql import MySQL
+from models import mysql, shorten, is_url, url_of, app, randomize
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        long = request.form.get('URL')
+
+        short = randomize()
+
+        res = shorten(long, short)
+
+        return render_template('index.html', short=short if res else 'error. Retry')
+
+    print(request.remote_addr)
+    return render_template('index.html')
+    
+@app.route('/<route>')
+def redir(route):
+    if is_url(route):
+        return redirect(url_of(route), code=302)
+    else:
+        return render_template('404.html', code=404)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
--- models.py
+++ models.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template, redirect, request
 from flaskext.mysql import MySQL
+from flask import Flask
 import string
 import random
 
 app = Flask(__name__)
-
-
 app.config['MYSQL_DATABASE_HOST'] = "remotemysql.com"
 app.config['MYSQL_DATABASE_PORT'] = 3306
 app.config['MYSQL_DATABASE_USER'] = "d2amZpLtuY"
@@ -15,12 +13,12 @@ mysql = MySQL()
 mysql.init_app(app)
 
 
-def return_one(cmd):
+def return_single(cmd):
     conn =  mysql.connect()
     cursor = conn.cursor()
     
     try:
-        res = cursor.execute(cmd)
+        cursor.execute(cmd)
     except Exception as e:
         print("!!!error", e)
         return False
@@ -30,10 +28,10 @@ def return_one(cmd):
     return cursor.fetchone()[0]
 
 def is_url(url):
-    return return_one(f'select count(*) from urls where shorturl="{url}"') >= 1
+    return return_single(f'select count(*) from urls where shorturl="{url}"') >= 1
 
 def url_of(short):
-    return return_one(f"SELECT longurl from urls where shorturl='{short}'")
+    return return_single(f"SELECT longurl from urls where shorturl='{short}'")
 
 def shorten(long, short):
     conn =  mysql.connect()
@@ -50,21 +48,6 @@ def shorten(long, short):
         
     return res == 1
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        long = request.form.get('URL')
+def randomize(includes: list=[string.ascii_lowercase, string.ascii_uppercase, string.digits]):
 
-        short = ''.join([[i for i in random.choice([i for i in random.choice([string.ascii_lowercase, string.ascii_uppercase, string.digits])])][0] for _ in range(6)])
-
-        res = shorten(long, short)
-
-        return render_template('index.html', short=short if res else 'error. Retry')
-    return render_template('index.html')
-    
-@app.route('/<route>')
-def redir(route):
-    if is_url(route):
-        return redirect(url_of(route), code=302)
-    else:
-        return render_template('404.html', code=404)
+    return ''.join([[i for i in random.choice([i for i in random.choice(includes)])][0] for _ in range(6)])
